cf8fdff2b3f49df4e0be4e0bc93bb6c643122a77
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-30 17:51:40 -0400

    added a bunch of classes and my noisemaker interface, the beginnings

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
@@ -0,0 +1,8 @@
+package com.zipcodewilmington.froilansfarm;
+
+class Animal implements NoiseMaker{ //, also implements Eater, just put a comma after NoiseMaker
+
+    public String makeNoise() {
+        return null;
+    }
+}
