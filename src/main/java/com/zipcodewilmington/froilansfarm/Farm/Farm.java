caa9dbd1f4c439f69a54faccf00356db846818c8
Zachary <zachary@Zipcoders-MBP-2.fios-router.home>
2023-03-30 22:26:20 -0400

    commit before bed

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
@@ -1,4 +1,8 @@
 package com.zipcodewilmington.froilansfarm.Farm;
 
+import com.zipcodewilmington.froilansfarm.FarmVehicle.FarmVehicle;
+
 public class Farm {
+    public Farm(Field field, ChickenCoop coop1, ChickenCoop coop2, ChickenCoop coop3, ChickenCoop coop4, Stable stable1, Stable stable2, Stable stable3, FarmVehicle vehicle1, FarmVehicle vehicle2) {
+    }
 }
