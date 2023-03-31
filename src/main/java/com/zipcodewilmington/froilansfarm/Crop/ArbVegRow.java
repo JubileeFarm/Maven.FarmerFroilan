bd0fdbbcd4698121e92aaf5f7d45420d46c543c3
Zachary <zachary@Zipcoders-MBP-2.fios-router.home>
2023-03-30 20:43:50 -0400

    esatablished the @before test for farm and created corn, tomato and arb veg class broccoli

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crop/ArbVegRow.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.froilansfarm.Crop;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class ArbVegRow extends ArrayList<Crop> {
+}
--- src/test/java/com/zipcodewilmington/froilansfarm/FarmTest/FarmTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmTest/FarmTest.java
@@ -1,10 +1,52 @@
 package com.zipcodewilmington.froilansfarm.FarmTest;
 
+import com.zipcodewilmington.froilansfarm.Crop.ArbVegRow;
+import com.zipcodewilmington.froilansfarm.Crop.CropRow;
+import com.zipcodewilmington.froilansfarm.Farm.ChickenCoop;
+import com.zipcodewilmington.froilansfarm.Farm.Field;
 import com.zipcodewilmington.froilansfarm.Farm.Stable;
-import org.junit.Assert;
+import com.zipcodewilmington.froilansfarm.FarmVehicle.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.Interfaces.Aircraft;
+import com.zipcodewilmington.froilansfarm.Person.Farmer;
+import com.zipcodewilmington.froilansfarm.Person.Maverick;
+import com.zipcodewilmington.froilansfarm.Produce.CornStalk;
+import com.zipcodewilmington.froilansfarm.Produce.TomatoPlant;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FarmTest {
+    private Field field;
+    private CropRow row1, row2;
+    private ArbVegRow row3;
+    private ChickenCoop coop1, coop2, coop3, coop4;
+    private Stable stable1, stable2, stable3;
+    private FarmVehicle vehicle1, vehicle2;
+    private Aircraft aircraft;
+    private Maverick pilot;
+    private Farmer farmer;
+
+    @Before //before class is used to execute before all tests are ran
+    public void setUp() {
+       //establishing the farm to test based on setting the plot in readMe
+        field = new Field();
+        row1 = new CropRow(CornStalk);
+        row2 = new CropRow(TomatoPlant);
+        row3 = new ArbVegRow();
+        coop1 = new ChickenCoop(4);
+        coop2 = new ChickenCoop(4);
+        coop3 = new ChickenCoop(4);
+        coop4 = new ChickenCoop(3);
+        stable1 = new Stable(4);
+        stable2 = new Stable(4);
+        stable3 = new Stable(2);
+        vehicle1 = new FarmVehicle();
+        vehicle2 = new FarmVehicle();
+        aircraft = new Aircraft();
+        pilot = new Pilot("Froilanda", aircraft);
+        farmer = new Farmer("Froilan", field, coop1, coop2, coop3, coop4, stable1, stable2, stable3, vehicle1, vehicle2);
+    }
+
+
 
     @Test
     public void TestAddStable(){
