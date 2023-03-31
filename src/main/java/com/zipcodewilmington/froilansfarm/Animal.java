ac52dd3f96a135710d2f479b466f678d585c859f
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-30 20:45:02 -0400

    i think i finished my classes, have to pull to check and see if everything works

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
@@ -1,8 +1,17 @@
 package com.zipcodewilmington.froilansfarm;
 
-class Animal implements NoiseMaker{ //, also implements Eater, just put a comma after NoiseMaker
-
+class Animal implements NoiseMaker{
+    @Override
     public String makeNoise() {
         return null;
+    } //, also implements Eater, just put a comma after NoiseMaker
+
+//    public String makeNoise() {
+//        public String makeNoise{
+//            return "noise";
+//        }
+//
+//        public String eat(Edible edible){
+//            return "yummy!";
+//        }
     }
-}
--- src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
@@ -1,4 +1,8 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Horse extends Animal { //implements Rideable
+
+    public String makeNoise(){
+        return "Neeeeeighhh";
+    }
 }
