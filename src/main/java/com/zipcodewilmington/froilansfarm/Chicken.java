53a1d8f221c822ae987ffa665624232537ba0c74
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-30 20:45:02 -0400

    i think i finished my classes, have to pull to check and see if everything works

--- src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -5,11 +5,28 @@ import java.util.ArrayList;
 public class Chicken extends Animal{ //implements Produce
 
     private boolean hasBeenFertilized;
+    private boolean hasBeenCollected;
     private String EdibleEgg;
+    private ArrayList<Egg> eggCollected;
 
     public Chicken(){
-        if (hasBeenFertilized == true)
+        this.eggCollected = new ArrayList<>();
         this.hasBeenFertilized = false;
+    }
+
+    public String makeNoise(){
+        return "Cluck cluck";
+    }
+
+    public ArrayList yield(){
+        hasBeenCollected = true;
+        ArrayList<Egg> eggs = (ArrayList<Egg>) this.eggCollected.clone();
+        this.eggCollected.clear();
+        return eggs;
+    }
+
+    public void beFertilized(){
+        this.hasBeenFertilized = true;
     }
 
 }
